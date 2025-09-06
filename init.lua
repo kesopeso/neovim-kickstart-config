9b77054cddc86d5c9c97e3a9a820b3748482b2fe
Dorde Kesic <dordekesic.keso@gmail.com>
2025-09-06 07:21:21 +0200

    adds prettier to react, html, json,... filetypes

--- init.lua
+++ init.lua
@@ -784,6 +784,12 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
