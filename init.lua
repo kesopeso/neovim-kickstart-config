98fe225213e114c9362f25dcd1a8b01501c41486
Dorde Kesic <dordekesic.keso@gmail.com>
2026-03-10 12:25:18 +0100

    adds go and gomod treesitter parsers, adds gopls language server initialization

--- init.lua
+++ init.lua
@@ -704,7 +704,7 @@ do
   ---@type table<string, vim.lsp.Config>
   local servers = {
     -- clangd = {},
-    -- gopls = {},
+    gopls = {},
     -- pyright = {},
     -- rust_analyzer = {},
     --
@@ -926,7 +926,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'gomod' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
