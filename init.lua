1d903e6d6179f21eac9459680d696731a41305a4
Dorde Kesic <dordekesic.keso@gmail.com>
2026-03-11 11:07:45 +0100

    adds yaml parser to tree-sitter

--- init.lua
+++ init.lua
@@ -614,6 +614,7 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       ---@type table<string, vim.lsp.Config>
       local servers = {
+        yamlls = {},
         -- clangd = {},
         gopls = {},
         -- pyright = {},
@@ -894,7 +895,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'gomod' }
+      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'gomod', 'yaml' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
